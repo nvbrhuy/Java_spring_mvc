2ca7c3a4db3e85579e1eca6dc2202e4f13b51c81
Nguyen Van Bao Huy <mbhuy2601@gmail.com>
2025-12-18 06:21:44 +0700

    push

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,6 +23,9 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
+        List<User> arrUser = this.userService.getAllUsersByEmail("admin@gmail.com");
+        System.out.println(arrUser);
+
         String test = this.userService.handleHello();
         model.addAttribute("eric", test);
         model.addAttribute("hoidanit", "from controller with model");
--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package vn.hoidanit.laptopshop.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +10,8 @@ import vn.hoidanit.laptopshop.domain.User;
 
 //crud: create, read, upadate, delete
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User save(User hoidanit);
+
+    List<User> findByEmail(String email);
 }
