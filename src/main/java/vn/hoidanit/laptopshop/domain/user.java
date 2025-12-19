9e28585c4c7478a2e8d0cf8e4e6666830fa24632
Nguyen Van Bao Huy <mbhuy2601@gmail.com>
2025-12-19 17:58:00 +0700

    push

--- src/main/java/vn/hoidanit/laptopshop/domain/user.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/user.java
@@ -19,6 +19,10 @@ public class User {
     private String address;
     private String phone;
 
+    private String avatar;
+
+    // RoleId
+
     public long getId() {
         return id;
     }
@@ -67,10 +71,18 @@ public class User {
         this.phone = phone;
     }
 
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
 
 }
